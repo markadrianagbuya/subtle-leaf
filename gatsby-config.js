11882ed510bece551436f0772ce988dedcf89658
Mark Agbuya <markadrianagbuya@gmail.com>
2019-06-16 16:47:31 +1000

    Change site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `Subtle Leaf`,
+    author: `Mark Agbuya`,
+    description: `Subtle Leaf Blog about house plants.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
       twitter: `kylemathews`,

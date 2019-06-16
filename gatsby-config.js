6a9f61ea904b42350ad355268e385e6e5f2b4a0e
Mark Agbuya <markadrianagbuya@gmail.com>
2019-06-16 21:55:00 +1000

    Remove feed plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,6 @@ module.exports = {
         //trackingId: `ADD YOUR TRACKING ID HERE`,
       },
     },
-    `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
